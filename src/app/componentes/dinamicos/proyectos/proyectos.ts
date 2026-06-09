3b4a10692a84572f6b60583716c717cda17df1c2
Fran Pacheco <franpacheco.cpp@gmail.com>
2026-06-09 14:47:23 -0300

    agregando interfces al servicio para mejor tipado, sintaxis con control flow de angular en proyectos.html, agregando campo de url a cada proyecto en db.json e implementacion de signals y logica oninit a proyectos.ts

--- src/app/componentes/dinamicos/proyectos/proyectos.ts
+++ src/app/componentes/dinamicos/proyectos/proyectos.ts
@@ -1,18 +1,32 @@
-import { Component } from '@angular/core';
-import { Proyecto } from '../../../servicios/proyecto';
+import { Component, signal, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProyectoServicio, ProyectoItem } from '../../../servicios/proyecto';
 
 @Component({
   selector: 'app-proyectos',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './proyectos.html',
   styleUrl: './proyectos.css',
 })
-export class Proyectos {
-  constructor(private proyecto: Proyecto) {
-    this.proyecto.obtenerProyectos().subscribe({
-      next: (data) => console.log(data),
-      error: (error) => console.log(error),
-      complete: () => console.info('completado'),
+export class Proyectos implements OnInit {
+  proyectos = signal<ProyectoItem[]>([]);
+  cargando = signal<boolean>(true);
+  error = signal<string | null>(null);
+
+  constructor(private proyectoServicio: ProyectoServicio) {}
+
+  ngOnInit(): void {
+    this.proyectoServicio.obtenerProyectos().subscribe({
+      next: (data) => {
+        this.proyectos.set(data);
+        this.cargando.set(false);
+      },
+      error: (err) => {
+        this.error.set('No se pudieron cargar los proyectos.');
+        this.cargando.set(false);
+        console.error(err);
+      },
+      complete: () => console.info('Proyectos cargados.'),
     });
   }
 }
--- src/app/servicios/proyecto.ts
+++ src/app/servicios/proyecto.ts
@@ -2,14 +2,29 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface Tecnologia {
+  id: string;
+  nombre: string;
+}
+
+export interface ProyectoItem {
+  id: string;
+  nombre: string;
+  descripcion: string;
+  img: string;
+  url?: string;
+  tecnologias: Tecnologia[];
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class Proyecto {
-  urlApi: string = 'http://localhost:3000/proyectos';
+export class ProyectoServicio {
+  private urlApi: string = 'http://localhost:3000/proyectos';
+
   constructor(private http: HttpClient) {}
 
-  obtenerProyectos(): Observable<any> {
-    return this.http.get(this.urlApi);
+  obtenerProyectos(): Observable<ProyectoItem[]> {
+    return this.http.get<ProyectoItem[]>(this.urlApi);
   }
 }
