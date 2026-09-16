a2f537a35664ec8c829f2fdc32802b1198f86450
Fran Pacheco <franpacheco.cpp@gmail.com>
2026-09-16 08:48:03 -0300

    rectificando cambios menores en el backend

--- backend/proyectos/urls.py
+++ backend/proyectos/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import ProyectoViewSet, TecnologiaViewSet
 
 router = DefaultRouter()
@@ -8,4 +9,6 @@ router.register(r"tecnologias", TecnologiaViewSet, basename="tecnologia")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("auth/login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
